a30775fcd5a2bb729d496848246be48c3653cea6
André Dobbss <67844894+andredobbss@users.noreply.github.com>
2025-04-17 20:30:33 -0300

    criando método estático para gerar relatório

--- Services/ProductsService.cs
+++ Services/ProductsService.cs
@@ -1,7 +1,6 @@
-﻿using FastReport.Export.PdfSimple;
-using FastReport.Web;
-using FastReportAPI.Repositories.Interfaces;
+﻿using FastReportAPI.Repositories.Interfaces;
 using FastReportAPI.Services.Interfaces;
+using FastReportAPI.Utils;
 
 namespace FastReportAPI.Services;
 
@@ -25,59 +24,35 @@ public class ProductsService : IProductsService
     public MemoryStream GetProductsMemoryStreamDataTable()
     {
         string sqlDataTable = 
-                    @"
-                    SELECT 
-                    	P.[ProductID],
-                    	P.[ProductName],
-                    	C.[CategoryName],
-                    	P.[QuantityPerUnit],
-                    	P.[UnitPrice]
-                    FROM 
-                    	[dbo].[Products] P
-                    INNER JOIN 
-                    	[dbo].[Categories] C
-                    ON
-                    	P.CategoryID = C.CategoryID";
-
-       
-        WebReport webReport = new WebReport();
-
-        webReport.Report.Load(Path.Combine(_webHostEnvironment.ContentRootPath,"wwwroot/reports", "ProductsReport.frx"));
+                             @"
+                             SELECT 
+                             	P.[ProductID],
+                             	P.[ProductName],
+                             	C.[CategoryName],
+                             	P.[QuantityPerUnit],
+                             	P.[UnitPrice]
+                             FROM 
+                             	[dbo].[Products] P
+                             INNER JOIN 
+                             	[dbo].[Categories] C
+                             ON
+                             	P.CategoryID = C.CategoryID";
+                          
 
         var dt = _dataTableRepository.FillDataTable(sqlDataTable);
 
-        webReport.Report.RegisterData(dt, "Products");
-
-        webReport.Report.Prepare();
-
-        using MemoryStream ms = new MemoryStream();
-
-        webReport.Report.Export(new PDFSimpleExport(), ms);
-
-        ms.Position = 0;
+        var ms =  GenerateReport.GetMemoryReportStream("ProductsReport.frx", "Products", _webHostEnvironment, dt);
 
         return ms;
     }
 
     public async Task<MemoryStream> GetProductsMemoryStreamIEnumerable()
     {
-        string sqlIEnumerable = "SELECT * FROM ProductByCategoryName"; //view criada
-
-        WebReport webReport = new WebReport();
-
-        webReport.Report.Load(Path.Combine(_webHostEnvironment.ContentRootPath, "wwwroot/reports", "ProductsReport.frx"));
+        string sqlIEnumerable = "SELECT * FROM ProductByCategoryName"; //view criada no banco de dados
 
         var ie = await _ienumerableRepository.GetEnumerable(sqlIEnumerable);
 
-        webReport.Report.RegisterData(ie, "Products");
-
-        webReport.Report.Prepare();
-
-        using MemoryStream ms = new MemoryStream();
-
-        webReport.Report.Export(new PDFSimpleExport(), ms);
-
-        ms.Position = 0;
+        var ms = GenerateReport.GetMemoryReportStream("ProductsReport.frx", "Products", _webHostEnvironment, ie);
 
         return ms;
     }
---
+++ Utils/GenerateReport.cs
@@ -0,0 +1,32 @@
+﻿using FastReport.Export.PdfSimple;
+using FastReport.Web;
+using System.Data;
+
+namespace FastReportAPI.Utils;
+
+public static class GenerateReport
+{
+    public static MemoryStream GetMemoryReportStream<T>(string reportName, string dataSetName, IWebHostEnvironment webHostEnvironment, T data)
+    {
+        WebReport webReport = new();
+
+        webReport.Report.Load(Path.Combine(webHostEnvironment.ContentRootPath, "wwwroot/reports", reportName));
+
+        if (data is DataTable dt)
+            webReport.Report.RegisterData(dt, dataSetName);
+
+        if (data is IEnumerable<object> ie)
+            webReport.Report.RegisterData(ie, dataSetName);
+
+        webReport.Report.Prepare();
+
+        using MemoryStream ms = new();
+
+        webReport.Report.Export(new PDFSimpleExport(), ms);
+
+        ms.Position = 0;
+
+        return ms;
+    }
+
+}
