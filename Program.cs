f41c846552f1f8ca6b98a695e4e3b2fa97784f50
André dos Santos Dobbss de Carvalho <andredobbss@hotmail.com>
2025-05-23 14:24:25 -0300

    incluindo Swagger

--- Program.cs
+++ Program.cs
@@ -13,6 +13,13 @@ builder.Services.AddControllers();
 
 builder.Services.AddFastReport();
 
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "FastReportAPI", Version = "v1" });
+});
+
 string connection = builder.Configuration.GetConnectionString("DefaultConnection")!;
 
 builder .Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connection));
@@ -26,6 +33,12 @@ builder.Services.AddScoped<IEnumerableRepository, EnumerableRepository>();
 builder.Services.AddScoped<IProductsService, ProductsService>();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
